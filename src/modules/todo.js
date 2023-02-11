d4647023882221603fcb49faf1389639b1f29f28
Wissam Mansour <wissman77@gmail.com>
2023-02-11 20:27:41 +0200

    remove all class methods

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -7,15 +7,4 @@ export default class Todo {
     this.completed = false;
     this.id = Date.now();
   }
-
-  completeTodo() {
-    this.completed = true;
-  }
-
-  updateTodo(title, dueDate, description, priority) {
-    this.title = title;
-    this.dueDate = dueDate;
-    this.description = description;
-    this.priority = priority;
-  }
 }
