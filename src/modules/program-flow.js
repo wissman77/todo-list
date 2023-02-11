caac2d86f800420d8af45f9bfb567ce5505adbd1
Wissam Mansour <wissman77@gmail.com>
2023-02-11 20:30:11 +0200

    add all methods for program logic

--- src/modules/program-flow.js
+++ src/modules/program-flow.js
@@ -3,12 +3,24 @@ import ProjectManger from './projects-manager';
 import Todo from './todo';
 
 const flow = (() => {
-  const _addToLocalStorage = () => {
+  const _saveToLocalStorage = () => {
     localStorage.setItem('projects', JSON.stringify(ProjectManger.projects));
   };
 
   const _getProjectsFromLocalStorage = () => {
-    return JSON.parse(localStorage.getItem('projects'));
+    ProjectManger.projects = JSON.parse(localStorage.getItem('projects'));
+  };
+
+  const _findProjectIndex = (projectId) => {
+    return ProjectManger.projects.findIndex(
+      (project) => project.id === projectId
+    );
+  };
+
+  const _findTodoIndex = (projectIndex, todoId) => {
+    return ProjectManger.projects[projectIndex].todos.findIndex(
+      (todo) => todo.id === todoId
+    );
   };
 
   const loadProjects = () => {
@@ -16,7 +28,7 @@ const flow = (() => {
     if (!localProjects) {
       const demoProject = new Project('Demo Project');
       ProjectManger.projects.push(demoProject);
-      _addToLocalStorage();
+      _saveToLocalStorage();
       localProjects = ProjectManger.projects;
     } else {
       ProjectManger.projects = localProjects;
@@ -24,8 +36,75 @@ const flow = (() => {
     return localProjects;
   };
 
+  const addNewProject = (name) => {
+    _getProjectsFromLocalStorage();
+    ProjectManger.projects.push(new Project(name));
+    _saveToLocalStorage();
+  };
+
+  const deleteProject = (projectId) => {
+    _getProjectsFromLocalStorage();
+    const index = _findProjectIndex(projectId);
+    ProjectManger.projects.splice(index, 1);
+    _saveToLocalStorage();
+  };
+
+  const updateProject = (name, projectId) => {
+    _getProjectsFromLocalStorage();
+    const index = _findProjectIndex(projectId);
+    ProjectManger.projects[index].name = name;
+    _saveToLocalStorage();
+  };
+
+  const addNewTodo = (title, dueDate, description, priority, projectId) => {
+    _getProjectsFromLocalStorage();
+    const todo = new Todo(title, dueDate, description, priority);
+    const index = _findProjectIndex(projectId);
+    ProjectManger.projects[index].push(todo);
+    _saveToLocalStorage();
+  };
+
+  const deleteTodo = (todoId, projectId) => {
+    _getProjectsFromLocalStorage();
+    const projectIndex = _findProjectIndex(projectId);
+    const todoIndex = _findTodoIndex(projectIndex, todoId);
+    ProjectManger.projects[projectIndex].todos.splice(todoIndex, 1);
+    _saveToLocalStorage();
+  };
+
+  const updateTodo = (
+    { title, dueDate, description, priority },
+    projectId,
+    todoId
+  ) => {
+    _getProjectsFromLocalStorage();
+    const projectIndex = _findProjectIndex(projectId);
+    const todoIndex = _findTodoIndex(projectIndex, todoId);
+    const todo = ProjectManger.projects[projectIndex].todos[todoIndex];
+    todo.title = title;
+    todo.dueDate = dueDate;
+    todo.description = description;
+    todo.priority = priority;
+    _saveToLocalStorage();
+  };
+
+  const completeTodo = (todoId, projectId) => {
+    const projectIndex = _findProjectIndex(projectId);
+    const todoIndex = _findTodoIndex(projectIndex, todoId);
+    const todo = ProjectManger.projects[projectIndex].todos[todoIndex];
+    todo.completed = !todo.completed;
+    _saveToLocalStorage();
+  };
+
   return {
     loadProjects,
+    addNewProject,
+    deleteProject,
+    updateProject,
+    addNewTodo,
+    deleteTodo,
+    updateTodo,
+    completeTodo,
   };
 })();
 
