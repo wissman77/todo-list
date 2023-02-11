afa65f24caefb20d20dd41e6a7f09c14d826b165
Wissam Mansour <wissman77@gmail.com>
2023-02-11 17:48:18 +0200

    add completeTodo and updateTodo methods

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -7,4 +7,15 @@ export default class Todo {
     this.completed = false;
     this.id = Date.now();
   }
+
+  completeTodo() {
+    this.completed = true;
+  }
+
+  updateTodo(title, dueDate, description, priority) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+  }
 }
