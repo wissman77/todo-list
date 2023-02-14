11649c28c312a4bceb292726d68db8ff75c64e47
Wissam Mansour <wissman77@gmail.com>
2023-02-14 16:13:35 +0200

    add methods toggleComplete update and renderHTML

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,6 @@
+import { endOfYesterday, isBefore } from 'date-fns';
+import ProjectManager from './projects-manager';
+
 export default class Todo {
   constructor(title, dueDate, description, priority) {
     this.title = title;
@@ -12,10 +15,93 @@ export default class Todo {
     this.completed = !this.completed;
   }
 
-  update(newTitle, newDueDate, newDescription, newPriority) {
+  update({ newTitle, newDueDate, newDescription, newPriority }) {
     this.title = newTitle;
     this.dueDate = newDueDate;
     this.description - newDescription;
     this.priority = newPriority;
+  }
+
+  render() {
+    const container = document.createElement('div');
+    container.classList.add('todo-container');
+
+    const todoBody = document.createElement('div');
+    todoBody.classList.add('todo-body');
+    container.appendChild(todoBody);
+
+    const completed = this.renderCheckbox();
+    completed.addEventListener('click', () => {
+      this.completed();
+      this.parentElement.classList.toggle('completed');
+    });
+    todoBody.appendChild(completed);
+    if (this.completed) todoBody.classList.add('completed');
+
+    const titlePara = document.createElement('p');
+    titlePara.classList.add('todo-title');
+    titlePara.textContent = this.title;
+    todoBody.appendChild(titlePara);
+
+    const descriptionPara = document.createElement('p');
+    descriptionPara.classList.add('todo-description');
+    descriptionPara.textContent = this.description;
+    todoBody.appendChild(descriptionPara);
+
+    const dueDatePara = document.createElement('div');
+    dueDatePara.classList.add('todo-duedate');
+    dueDatePara.textContent = format(new Date(this.dueDate), 'dd-MM-yyyy');
+    todoBody.appendChild(dueDatePara);
+    if (isBefore(new Date(this.dueDate), endOfYesterday())) {
+      dueDatePara.classList.add('expired');
+      dueDatePara.textContent += ' Expired';
+    }
+
+    const projectNamePara = document.createElement('p');
+    projectNamePara.classList.add('todo-project-name');
+    projectNamePara.textContent = ProjectManager.getProjectName(this.id);
+    todoBody.appendChild(projectNamePara);
+
+    const actions = document.createElement('div');
+    actions.classList.add('actions');
+    container.appendChild(actions);
+
+    const priorityFlag = document.createElement('i');
+    priorityFlag.classList.add('fa-solid', 'fa-flag');
+    priorityFlag.classList.add(this.priority);
+    priorityFlag.title = this.priority;
+    actions.appendChild(priorityFlag);
+
+    const editTodoFlag = document.createElement('i');
+    editTodoFlag.classList.add('fa-solid', 'fa-pen-to-square');
+    editTodoFlag.setAttribute('data-todoid', this.id);
+    // editTodoFlag.addEventListener('click', () => {
+    //   enterEditTodoMode(todo.id, projectId);
+    //   todoModal.style.display = 'block';
+    // });
+    actions.appendChild(editTodoFlag);
+
+    const deleteTodoFlag = document.createElement('i');
+    deleteTodoFlag.classList.add('fa-solid', 'fa-trash', 'danger');
+    deleteTodoFlag.setAttribute('data-todoid', this.id);
+    // deleteTodoFlag.addEventListener(
+    //   'click',
+    //   deleteTodo.bind(this, todo.id, projectId)
+    // );
+    actions.appendChild(deleteTodoFlag);
+
+    todosList.appendChild(container);
+
+    // return the container for todo object
+    return container;
+  }
+
+  renderCheckbox() {
+    const checkbox = doc.createElement('input');
+    checkbox.type = 'checkbox';
+    if (this.completed) {
+      checkbox.checked = true;
+    }
+    return checkbox;
   }
 }
