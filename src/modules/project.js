a76be485bc0e665d16ad448506c81cd3200c94ae
Wissam Mansour <wissman77@gmail.com>
2023-02-14 16:51:33 +0200

    add methods removeTodo addTodo and renderHTML

--- src/modules/project.js
+++ src/modules/project.js
@@ -4,4 +4,43 @@ export default class Project {
     this.id = Date.now().toString();
     this.todos = [];
   }
+
+  _findTodoIndex(todoId) {
+    return this.todos.findIndex((todo) => todo.id === todoId);
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  removeTodo(todoId) {
+    const index = this._findTodoIndex(todoId);
+    this.todos.splice(index, 1);
+  }
+
+  searchTodo(todoId) {
+    const index = this._findTodoIndex(todoId);
+    if (index > -1) {
+      return this.todos[index];
+    }
+    return null;
+  }
+
+  renderHTML() {
+    const element = `
+    <li>
+      <a href="#" data-projectid="${
+        this.id
+      }"><i class="fa-solid fa-list-check"></i>&nbsp;${this.name}</a>
+        ${
+          this.name !== 'Demo Project'
+            ? `<button class="btn"><i data-projectid="${this.id}" class="fa-solid fa-delete-left"></i></button>
+              <button class="btn"><i data-projectid="${this.id}" class="fa-solid fa-pen-to-square"></i></button>`
+            : ``
+        }
+    </li>
+    `;
+
+    return element;
+  }
 }
